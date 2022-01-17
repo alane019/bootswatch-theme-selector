b627b4ade452c55764e8c35a7632f92859c3c8b3
Adam Lane <alane019@gmail.com>
2022-01-17 17:55:14 -0500

    update footer content, App.js text formatting, and App.css

--- src/App.js
+++ src/App.js
@@ -1,194 +1,193 @@
-import "./App.css";																		
-import axios from "axios";																		
-import React, { useEffect, useState } from "react";																		
-import BootswatchDemo from "./components/BootswatchDemo";																		
-import Footer from "./components/Footer";																		
-import Header from "./components/Header";																		
-import { Container } from "react-bootstrap";																		
-import "bootstrap/dist/css/bootstrap.min.css";																																
-import { DropdownButton, Dropdown } from "react-bootstrap";																		
-																
-const apiEndpoint = "https://bootswatch.com/api/5.json";																		
-																		
-function App() {																		
-	const [theme, setTheme] = useState("");																																																
-	const [themeList, setThemeList] = useState([]);																	
-	const [themeHref, setThemeHref] = useState("");		
+import "./App.css";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import BootswatchDemo from "./components/BootswatchDemo";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import { Container } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { DropdownButton, Dropdown } from "react-bootstrap";
 
-																												
-	let handleSelectChange = (e) => {																
-		console.log("e param in handle select change fn: ", e);															
-		setTheme(e);																													
-		let hrefText =															
-			"https://cdn.jsdelivr.net/npm/bootswatch@5.1.1/dist/" +														
-			e +														
-			"/bootstrap.min.css";														
-		setThemeHref(hrefText);													
-	};			
-																																															
-	useEffect(() => { 																
-			axios																
-				.get(apiEndpoint)															
-				.then((response) => {															
-					console.log(response.data);														
-					console.log("response.data.themes: ", response.data.themes);																																									
-					if (theme){														
-						setThemeHref(													
-						"https://cdn.jsdelivr.net/npm/bootswatch@5.1.1/dist/" +												
-								theme +											
-								"/bootstrap.min.css"
-						);													
-					}																																										
-					let themes = [];
-					for (let i = 0; i < response.data.themes.length; i++) {														
-						let themeData = {													
-							name: response.data.themes[i].name.toLowerCase(),												
-							css: response.data.themes[i].css,												
-							cssCdn: response.data.themes[i].cssCdn,												
-							cssMin: response.data.themes[i].cssMin,												
-							description: response.data.themes[i].description,												
-							preview: response.data.themes[i].preview,												
-							scss: response.data.themes[i].scss,												
-							scssVariables: response.data.themes[i].scssVariables,												
-							thumbnail: response.data.themes[i].thumbnail,												
-						};																																						
-						themes.push(themeData);													
-					}																											
-					setThemeList(themes);																																				
-				})															
-				.catch((error) => {															
-					console.error(error);														
-				});																																
-	}, []) // eslint-disable-line react-hooks/exhaustive-deps		
-		
-	let getThemeDescription = () => {																																								
+const apiEndpoint = "https://bootswatch.com/api/5.json";
+
+function App() {
+	const [theme, setTheme] = useState("");
+	const [themeList, setThemeList] = useState([]);
+	const [themeHref, setThemeHref] = useState("");
+
+	let handleSelectChange = (e) => {
+		setTheme(e);
+		let hrefText =
+			"https://cdn.jsdelivr.net/npm/bootswatch@5.1.1/dist/" +
+			e +
+			"/bootstrap.min.css";
+		setThemeHref(hrefText);
+	};
+
+	useEffect(() => {
+		axios
+			.get(apiEndpoint)
+			.then((response) => {
+				if (theme) {
+					setThemeHref(
+						"https://cdn.jsdelivr.net/npm/bootswatch@5.1.1/dist/" +
+							theme +
+							"/bootstrap.min.css"
+					);
+				}
+				let themes = [];
+				for (let i = 0; i < response.data.themes.length; i++) {
+					let themeData = {
+						name: response.data.themes[i].name.toLowerCase(),
+						css: response.data.themes[i].css,
+						cssCdn: response.data.themes[i].cssCdn,
+						cssMin: response.data.themes[i].cssMin,
+						description: response.data.themes[i].description,
+						preview: response.data.themes[i].preview,
+						scss: response.data.themes[i].scss,
+						scssVariables: response.data.themes[i].scssVariables,
+						thumbnail: response.data.themes[i].thumbnail,
+					};
+					themes.push(themeData);
+				}
+				setThemeList(themes);
+			})
+			.catch((error) => {
+				console.error(error);
+			});
+	}, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+	let getThemeDescription = () => {
 		let themeDescr;
-		if(themeList[0]){															
-			let index = themeList.findIndex((item) => item.name === theme);														
-			themeDescr = themeList[index].description;	
+		if (themeList[0]) {
+			let index = themeList.findIndex((item) => item.name === theme);
+			themeDescr = themeList[index].description;
 		}
-		return themeDescr;													
-	};		
-																													
-	var i = 0;																																		
-	let themeArray = themeList;																	
-			
+		return themeDescr;
+	};
 
-	return(																																	
-		<Container>																
-			{/* <link href={"https://cdn.jsdelivr.net/npm/bootswatch@5.1.1/dist/" + theme + "/bootstrap.min.css"} rel="stylesheet" crossOrigin="anonymous" /> */}															
-			{theme ? (															
-				<link														
-					href={themeHref}													
-					type="text/css"													
-					rel="stylesheet"													
-					crossOrigin="anonymous"													
-				/>														
-			) : (	<span> </span>														
-																		
-			)}															
-			<Header />															
-																		
-			{/* ////////////////////////////////////////////// */}															
-																		
-			<div className="container">															
-				<br />														
-				<div className="page-header select-theme" id="banner">														
-				<div className="row">
-					
-				<div id="page-description" style={{fontSize: "1.2rem"}}>	
-				<a href="https://bootswatch.com/">Bootswatch</a> is collection 
-				of open source themes for <a href="https://getbootstrap.com/">Bootstrap</a>.
-				{"  "} Bootswatch Theme Selector allows you to preview each theme on a single page and 
-				includes the link tag for the selected theme. 
-				</div>	
+	var i = 0;
+	let themeArray = themeList;
 
-				{theme ? "" : <b className="fade-in">
-				  <div className="mt-2" ><span className="badge bg-secondary">Use the drop down menu below to select a theme. </span>
-				  </div></b> }
-														
-						<div className="col-lg-8 col-md-7 col-sm-6">												
-							{theme ? (											
-								<h1> {theme} </h1>										
-							) : (											
-								<div id="default">										
-									<span> Default theme </span>									
-									<span className="not-bold"> (Bootstrap 4.6.1) </span>									
-								</div>										
-							)}											
-							<p className="lead">											
-								{ theme										
-									?  getThemeDescription()									
-									: "Select a Bootswatch theme."									
-								}										
-							</p>											
-						</div>												
-						<div className="col-lg-4 col-md-5 col-sm-6"></div>												
-					</div>													
-				</div>														
-																		
-																		
-								<DropdownButton										
-									onSelect={handleSelectChange} 
-									title={ theme ? theme : "Select a Theme"}									
-									id="theme-select-dropdown"																	
-								>										
-									{themeArray.map((arrayItem) => {									
-										return (								
-											<Dropdown.Item							
-												className="dropdown-item"						
-												key={"item-" + i++}						
-												 eventKey={arrayItem.name}						
-												value={arrayItem.name}						
-												data-value={arrayItem.name}						
-											>							
-												{arrayItem.name}						
-											</Dropdown.Item>							
-										);								
-									})}									
-							</DropdownButton>											
-																		
-					{/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */}													
-																													
-																		
-				{ theme ? (														
-								<div										
-									className="fade-in card text-black bg-light mb-3"									
-									style={{ maxWidth: "95%" }}									
-								>										
-									<div className="card-body">	
-									<h3 id="cdn-description">
-										Use the CDN link tag below or visit the {" "}
-										<a href={'https://bootswatch.com/' + theme + '/'}
-										rel="noopener noreferrer" target="_blank">
-										{theme} theme</a> {" "} page to download.
-									</h3>
-									<div className="card bg-info mb-2"
-											style={{maxWidth: "80%",
-											margin: "auto"}}>	
-											<div className="card-body">	
-												<div className="text-white"> 
-												{'<link href="' + themeHref + '" type="text/css" rel="stylesheet" crossOrigin="anonymous" />'}
-											 </div>	
-											</div>								
-										</div>	
-								    </div>								
-								</div>										
-				) :  "" }														
-																		
-																		
-			</div>															
-																		
-			<hr></hr>															
-																		
-			<BootswatchDemo />															
-																		
-			{/* ////////////////////////////////////////////////// */}															
-																		
-			<Footer/>															
-	 </Container>																	
-  );																		
-  																		
-}																		
-																		
-export default App;	
+	return (
+		<Container>
+			{theme ? (
+				<link
+					href={themeHref}
+					type="text/css"
+					rel="stylesheet"
+					crossOrigin="anonymous"
+				/>
+			) : (
+				<span> </span>
+			)}
+			<Header />
+
+			<div className="container">
+				<div className="page-header select-theme" id="banner">
+					<div className="row">
+						<div id="page-description" style={{ fontSize: ".9rem" }}>
+							<a href="https://bootswatch.com/">Bootswatch</a> is collection of
+							open source themes for{" "}
+							<a href="https://getbootstrap.com/">Bootstrap</a>.{"  "}{" "}
+							Bootswatch Theme Selector allows you to preview each theme on a
+							single page and includes a link tag for the selected theme.
+						</div>
+
+						{theme ? (
+							""
+						) : (
+							<div className="mt-2 fade-in font-weight-bold">
+								<span className="badge bg-secondary">
+									Use the drop down menu below to select a theme.{" "}
+								</span>
+							</div>
+						)}
+
+						<div className="col-lg-8 col-md-7 col-sm-6">
+							{theme ? (
+								<h1> {theme} </h1>
+							) : (
+								<div id="default">
+									<span> Default theme </span>
+									<span className="not-bold"> (Bootstrap 4.6.1) </span>
+								</div>
+							)}
+							<p className="lead">
+								{theme ? getThemeDescription() : "Select a Bootswatch theme."}
+							</p>
+						</div>
+						<div className="col-lg-4 col-md-5 col-sm-6"></div>
+					</div>
+				</div>
+
+				<DropdownButton
+					onSelect={handleSelectChange}
+					title={theme ? theme : "Select a Theme"}
+					id="theme-select-dropdown"
+				>
+					{themeArray.map((arrayItem) => {
+						return (
+							<Dropdown.Item
+								className="dropdown-item"
+								key={"item-" + i++}
+								eventKey={arrayItem.name}
+								value={arrayItem.name}
+								data-value={arrayItem.name}
+							>
+								{arrayItem.name}
+							</Dropdown.Item>
+						);
+					})}
+				</DropdownButton>
+
+				{theme ? (
+					<div
+						className="fade-in card text-black bg-light mb-3"
+						style={{ maxWidth: "95%" }}
+					>
+						<div
+							className="card-body"
+							style={{ textShadow: "rgb(99,99,99) 1px 1px 1px" }}
+						>
+							<h3 id="cdn-description">
+								Use the CDN link tag below or visit the{" "}
+								<a
+									href={"https://bootswatch.com/" + theme + "/"}
+									style={{ textDecoration: "none" }}
+									rel="noopener noreferrer"
+									target="_blank"
+								>
+									{theme} theme
+								</a>{" "}
+								page to download.
+							</h3>
+							<div
+								className="card bg-info mb-2"
+								style={{ maxWidth: "80%", margin: "auto" }}
+							>
+								<div className="card-body">
+									<div className="text-white">
+										{'<link href="' +
+											themeHref +
+											'" type="text/css" rel="stylesheet" crossOrigin="anonymous" />'}
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				) : (
+					""
+				)}
+			</div>
+
+			<hr></hr>
+
+			<BootswatchDemo />
+
+			<Footer />
+		</Container>
+	);
+}
+
+export default App;
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,98 +1,123 @@
-import React from 'react';
-import Box from '@material-ui/core/Box';
-import Container from '@material-ui/core/Container';
-import Divider from '@material-ui/core/Divider';
-import Typography from '@material-ui/core/Typography';
-import Widgets from '@material-ui/icons/Widgets';
-import { ColumnToRow, Row, Item } from '@mui-treasury/components/flex';
-import { NavMenu, NavItem } from '@mui-treasury/components/menu/navigation';
-
+import React from "react";
+import Box from "@material-ui/core/Box";
+import Container from "@material-ui/core/Container";
+import Divider from "@material-ui/core/Divider";
+import Typography from "@material-ui/core/Typography";
+import Widgets from "@material-ui/icons/Widgets";
+import { ColumnToRow, Row, Item } from "@mui-treasury/components/flex";
+import { NavMenu, NavItem } from "@mui-treasury/components/menu/navigation";
 import {
-  SocialProvider,
-  SocialLink,
-} from '@mui-treasury/components/socialLink';
+	SocialProvider,
+	SocialLink,
+} from "@mui-treasury/components/socialLink";
 
- const Footer = React.memo(function NeptuneAppFooter() {
-  return (
-    <Box bgcolor={'background.paper'} width={'100%'}>
-      <Container>
-        <Box pt={8} pb={2}>
-          <Row wrap>
-            <Item grow={2}>
-              <Row alignItems={'center'}>
-                <Item color={'#007bff'} fontSize={64} lineHeight={0}>
-                  <Widgets fontSize={'inherit'} color={'inherit'} />
-                </Item>
-                <Item>
-                  <Typography variant={'h6'} color={'textSecondary'}>
-                    Bootswatch Theme Selector
-                  </Typography>
-                </Item>
-              </Row>
-              <NavMenu>
-                <NavItem>About</NavItem>
-                <NavItem>Careers</NavItem>
-                <NavItem>Press</NavItem>
-                <NavItem>Customer Care</NavItem>
-                <NavItem>Services</NavItem>
-              </NavMenu>
-            </Item>
-            <Item grow maxWidth={500} mx={'auto'}>
-              <Box textAlign={'center'} mt={{ xs: 2, md: 0 }} my={2}>
-                <SocialProvider >
-                  <SocialLink
-                    brand={'FacebookCircle'}
-                    href={'https://github.com/alane019'}
-                  />
-                  <SocialLink
-                    brand={'Twitter'}
-                    href={'https://github.com/alane019'}
-                  />
-                  <SocialLink brand={'Instagram'} href={'https://github.com/alane019'} />
-                </SocialProvider>
-              </Box>
-            </Item>
-          </Row>
-        </Box>
-        <Divider />
-        <Box pt={2} pb={10}>
-          <ColumnToRow
-            at={'md'}
-            columnStyle={{ alignItems: 'center' }}
-            rowStyle={{ alignItems: 'unset' }}
-          >
-            <Item grow ml={-2} shrink={0}>
-              <NavMenu >
-                <ColumnToRow at={'sm'}>
-                  <NavItem>
-                    Terms &amp; Conditions
-                  </NavItem>
-                  <NavItem>
-                    Privacy Policy
-                  </NavItem>
-                  <NavItem>
-                    Accessibility
-                  </NavItem>
-                  <NavItem>Legal</NavItem>
-                </ColumnToRow>
-              </NavMenu>
-            </Item>
-            <Item>
-              <Box py={1} textAlign={{ xs: 'center', md: 'right' }}>
-                <Typography
-                  component={'p'}
-                  variant={'caption'}
-                  color={'textSecondary'}
-                >
-                  Bootswatch Theme Selector |  &copy; 2022 All rights reserved
-                </Typography>
-              </Box>
-            </Item>
-          </ColumnToRow>
-        </Box>
-      </Container>
-    </Box>
-  );
+const Footer = React.memo(function NeptuneAppFooter() {
+	return (
+		<Box bgcolor={"background.paper"} width={"100%"}>
+			<Container>
+				<Box pt={8} pb={2}>
+					<Row wrap>
+						<Item grow={2}>
+							<Row alignItems={"center"}>
+								<Item color={"#007bff"} fontSize={64} lineHeight={0}>
+									<Widgets fontSize={"inherit"} color={"inherit"} />
+								</Item>
+								<Item>
+									<Typography variant={"h6"} color={"textSecondary"}>
+										Bootswatch Theme Selector
+									</Typography>
+								</Item>
+							</Row>
+							<NavMenu>
+								<NavItem>About</NavItem>
+								<NavItem>Careers</NavItem>
+								<NavItem>Press</NavItem>
+								<NavItem>Customer Care</NavItem>
+								<NavItem>Services</NavItem>
+							</NavMenu>
+						</Item>
+						<Item grow maxWidth={500} mx={"auto"}>
+							<Box textAlign={"center"} mt={{ xs: 2, md: 0 }} my={2}>
+								<SocialProvider>
+									<SocialLink
+										brand={"FacebookCircle"}
+										href={"https://github.com/alane019"}
+									/>
+									<SocialLink
+										brand={"Twitter"}
+										href={"https://github.com/alane019"}
+									/>
+									<SocialLink
+										brand={"Instagram"}
+										href={"https://github.com/alane019"}
+									/>
+								</SocialProvider>
+							</Box>
+						</Item>
+					</Row>
+				</Box>
+				<Divider />
+				<Box pt={2} pb={10}>
+					<ColumnToRow
+						at={"md"}
+						columnStyle={{ alignItems: "center" }}
+						rowStyle={{ alignItems: "unset" }}
+					>
+						<Item grow ml={-2} shrink={0}>
+							<NavMenu>
+								<ColumnToRow at={"sm"}>
+									<NavItem>Terms &amp; Conditions</NavItem>
+									<NavItem>Privacy Policy</NavItem>
+									<NavItem>Accessibility</NavItem>
+									<NavItem>Legal</NavItem>
+								</ColumnToRow>
+							</NavMenu>
+						</Item>
+					</ColumnToRow>
+					<ColumnToRow
+						className="footerColToRow"
+						at={"md"}
+						columnStyle={{ alignItems: "left" }}
+						rowStyle={{ alignItems: "unset" }}
+					>
+						<Item>
+							<Box
+								id="footer-credits"
+								py={1}
+								textAlign={{ xs: "center", md: "center" }}
+							>
+								<Typography
+									id="footer-credits"
+									component={"p"}
+									variant={"caption"}
+									color={"textSecondary"}
+								>
+									Bootswatch Theme Selector Copyright &copy; 2022 Adam Lane is
+									released under the{" "}
+									<a href="https://raw.githubusercontent.com/alane019/bootswatch-theme-selector/main/LICENSE">
+										MIT License
+									</a>
+									.
+		
+									Bootswatch was created by{" "}
+									<a href="https://github.com/thomaspark">Thomas Park</a> under
+									the MIT license.
+
+									{" "}
+									Bootstrap is a collaborative open source project created and
+									maintained by the{" "}
+									<a href="https://getbootstrap.com/docs/5.1/about/team/">
+										Bootstrap Team
+									</a>{" "}
+									under the MIT license.
+								</Typography>
+							</Box>
+						</Item>
+					</ColumnToRow>
+				</Box>
+			</Container>
+		</Box>
+	);
 });
 
 export default Footer;
